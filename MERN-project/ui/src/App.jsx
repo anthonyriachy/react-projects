36e9569fad19ee7cb32bd0d661641cf7d30e9c0b
anthony riachy <anthony.riachy.39@gmail.com>
2023-07-12 23:38:20 +0300

    adding to db is fine now started file decomposition

---
+++ MERN-project/ui/src/App.jsx
@@ -0,0 +1,170 @@
+// const sampleIssue = {
+//   status: "New",
+//   owner: "Pieta",
+//   title: "Completion date should be optional",
+// };
+
+
+
+
+const dateRegex = new RegExp("^\\d\\d\\d\\d-\\d\\d-\\d\\d");
+function jsonDateReviver(key, value) {
+  if (dateRegex.test(value)) return new Date(value);
+  return value;
+}
+
+async function graphQLFetch(query, variables = {}) {
+  try {
+    const response = await fetch("/graphql", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ query, variables }),
+    });
+    const body = await response.text();
+    const result = JSON.parse(body, jsonDateReviver);
+    if (result.errors) {
+      const error = result.errors[0];
+      if (error.extensions.code == "BAD_USER_INPUT") {
+        const details = error.extensions.exception.errors.join("\n ");
+        alert(`${error.message}:\n ${details}`);
+      } else {
+        alert(`${error.extensions.code}: ${error.message}`);
+      }
+    }
+    return result.data;
+  } catch (e) {
+    alert(`Error in sending data to server: ${e.message}`);
+  }
+}
+
+class IssueAdd extends React.Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault(); //to stop it from submitting it
+    const form = document.forms.addForm;
+
+    const issue = {
+      owner: form.owner.value,
+      title: form.title.value,
+      due: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 10),
+    };
+    this.props.createIssue(issue);
+    form.owner.value = ""; //rest the input field
+    form.title.value = "";
+  }
+
+  render() {
+    return (
+      <form name="addForm" className="form-class" onSubmit={this.handleSubmit}>
+        {/* handleSubmit expects a event object (e) */}
+        <input type="text" name="owner" placeholder="Owner" />
+        <input type="text" name="title" placeholder="Title" />
+        <button>Add</button>
+      </form>
+    );
+  }
+}
+
+class IssueFilter extends React.Component {
+  render() {
+    return <div>IssueFilter</div>;
+  }
+}
+
+function IssueRow(props) {
+  return (
+    <tr>
+      <td>{props.issue.id}</td>
+      <td>{props.issue.status}</td>
+      <td>{props.issue.owner}</td>
+      <td>{props.issue.created.toDateString()}</td>
+      <td>{props.issue.effort}</td>
+      <td>{props.issue.due ? props.issue.due.toDateString() : ' '}</td>
+      <td>{props.issue.title}</td>
+    </tr>
+  );
+}
+
+function IssueTable(props) {
+  const issueRows = props.issues.map((issue) => (
+    <IssueRow key={issue.id} issue={issue} />
+  ));
+  return (
+    <table className="bordered-table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Status</th>
+          <th>Owner</th>
+          <th>Created</th>
+          <th>Effort</th>
+          <th>Due Date</th>
+          <th>Title</th>
+        </tr>
+      </thead>
+      <tbody>{issueRows}</tbody>
+    </table>
+  );
+}
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = { issues: [] };
+    this.createIssue = this.createIssue.bind(this); //!!!!!!!!!!111 this  is very important it won't work without it
+  }
+  async loadData() {
+    //   //check 123 very important
+    const query = `query {
+        issueList {
+        _id id title status owner
+        created effort due
+        }
+        }`;
+
+    
+    const data = await graphQLFetch(query);
+    if (data) {
+      this.setState({ issues: data.issueList });
+    }
+  }
+
+  componentDidMount() {
+    // see if the ui is loaded
+    this.loadData(); //if yes load the data
+  }
+
+  async createIssue(issue) {
+    
+    const query = `mutation issueAdd($issue: IssueInputs!) {
+        issueAdd(issue: $issue) {
+        id
+        }
+        }`;
+
+    const data = await graphQLFetch(query, { issue });
+    if (data) {
+      this.loadData();
+    }
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Issue Tracker</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTable issues={this.state.issues} />
+        <hr />
+        <IssueAdd createIssue={this.createIssue} /> {/* Add createIssue prop */}
+      </React.Fragment>
+    );
+  }
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
